7cf70acd3c38340308d4f0d24cc2082be70bcc76
Sam Meech-Ward <sam@meech-ward.me>
2023-01-04 11:58:17 -0800

    change port to env var

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.get("/burp", (req, res) => {
   })
 })
 
-app.listen(8080, () => {
-  console.log('Server started on port 8080')
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log('Server started on port', port)
 })
